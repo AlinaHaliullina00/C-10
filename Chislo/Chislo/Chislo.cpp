859c6248d6334efaef5197f18f5d854fea183aa8
AlinaHaliullina00 <101719855+AlinaHaliullina00@users.noreply.github.com>
2022-06-13 22:25:15 +0700

    Add files via upload

---
+++ Chislo/Chislo/Chislo.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include<fstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+	int i, n = 0;
+	float x;
+	ofstream file("F.txt");
+	cout << " Kolichestvo chisel ";
+	cin >> n;
+
+	for (i = 0; i < n; i++) {
+		cin >> x;
+		file << x << " ";
+	}
+	file.close();
+	ifstream vvod("F.txt");
+	float mas[100];
+	i = 0; {
+	do
+	{
+		vvod >> mas[i];
+		i++;
+	} while (!vvod.eof());
+}
+	vvod.close();
+	
+	int mas2[100];
+	for (i = 0; i < n; i++) {
+		mas2[i] = int(mas[i]);
+	};
+	ofstream file2("G.txt");
+	for (i = 0; i < n; i++) {
+		cin >> mas2[n];
+		file2 << mas2[i] << " ";
+	}
+		}
+
